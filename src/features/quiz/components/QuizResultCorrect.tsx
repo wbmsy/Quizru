6618cb860265127229e749d90e2ab848dd3055dd
wbmsy <177901080+wbmsy@users.noreply.github.com>
2025-02-18 20:29:56 +0900

    delete unnecessary motion

--- src/features/quiz/components/QuizResultCorrect.tsx
+++ src/features/quiz/components/QuizResultCorrect.tsx
@@ -1,5 +1,4 @@
 import { nextQuizSubmit } from "../nextQuizSubmit";
-import { motion } from "framer-motion";
 
 const QuizResultCorrect: React.FC<{
   db_id: string;
@@ -8,26 +7,16 @@ const QuizResultCorrect: React.FC<{
   const nextQuizSubmitWithValues = nextQuizSubmit.bind(null, db_id, num);
 
   return (
-    <motion.div
-      className="w-screen h-auto"
-      initial={{ opacity: 0, scale: 0.8 }}
-      animate={{ opacity: 1, scale: 1 }}
-      transition={{
-        duration: 0.8,
-        ease: "easeOut",
-      }}
-    >
-      <div className="flex flex-col bg-white rounded-lg shadow-xl p-8 w-[90%] max-w-[500px] mx-auto border border-gray-100">
-        <div className="text-center">
-          <h1 className="text-3xl font-bold text-green-500 mb-4">正解です！</h1>
-          <form action={nextQuizSubmitWithValues}>
-            <button className="inline-block text-white bg-green-500 hover:bg-green-600 py-2 px-4 rounded-lg text-lg font-semibold">
-              次の問題へ
-            </button>
-          </form>
-        </div>
+    <div className="flex flex-col bg-white rounded-lg shadow-xl p-8 w-[90%] max-w-[500px] mx-auto border border-gray-100">
+      <div className="text-center">
+        <h1 className="text-3xl font-bold text-green-500 mb-4">正解です！</h1>
+        <form action={nextQuizSubmitWithValues}>
+          <button className="inline-block text-white bg-green-500 hover:bg-green-600 py-2 px-4 rounded-lg text-lg font-semibold">
+            次の問題へ
+          </button>
+        </form>
       </div>
-    </motion.div>
+    </div>
   );
 };
 
--- src/features/quiz/components/QuizResultInCorrect.tsx
+++ src/features/quiz/components/QuizResultInCorrect.tsx
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { getQuizCorrectAnswer } from "../getDatabaseProperties";
 import { nextQuizSubmit } from "../nextQuizSubmit";
-import { motion } from "framer-motion";
 
 const QuizResultInCorrect: React.FC<{
   db_id: string;
@@ -20,33 +19,23 @@ const QuizResultInCorrect: React.FC<{
   const nextQuizSubmitWithValues = nextQuizSubmit.bind(null, db_id, num);
 
   return (
-    <motion.div
-      className="w-screen h-auto"
-      initial={{ opacity: 0, scale: 0.8 }}
-      animate={{ opacity: 1, scale: 1 }}
-      transition={{
-        duration: 0.8,
-        ease: "easeOut",
-      }}
-    >
-      <div className="flex flex-col bg-white rounded-lg shadow-xl p-8 w-[90%] max-w-[500px] mx-auto border border-gray-100">
-        <div className="text-center">
-          <h1 className="text-7xl font-bold text-red-500 mb-4">&times;</h1>
-          <p className="text-lg text-gray-700 mb-8">
-            残念！正しい答えは{" "}
-            <span className="inline-block font-bold underline decoration-wavy underline-offset-[6px] decoration-2 decoration-indigo-900 mt-1 mb-3 px-2">
-              {quizCorrectAnswer.join("、 ")}{" "}
-            </span>
-            です。
-          </p>
-          <form action={nextQuizSubmitWithValues}>
-            <button className="inline-block text-white bg-red-500 hover:bg-red-600 py-2 px-4 rounded-lg text-lg font-semibold">
-              次の問題へ
-            </button>
-          </form>
-        </div>
+    <div className="flex flex-col bg-white rounded-lg shadow-xl p-8 w-[90%] max-w-[500px] mx-auto border border-gray-100">
+      <div className="text-center">
+        <h1 className="text-7xl font-bold text-red-500 mb-4">&times;</h1>
+        <p className="text-lg text-gray-700 mb-8">
+          残念！正しい答えは{" "}
+          <span className="inline-block font-bold underline decoration-wavy underline-offset-[6px] decoration-2 decoration-indigo-900 mt-1 mb-3 px-2">
+            {quizCorrectAnswer.join("、 ")}{" "}
+          </span>
+          です。
+        </p>
+        <form action={nextQuizSubmitWithValues}>
+          <button className="inline-block text-white bg-red-500 hover:bg-red-600 py-2 px-4 rounded-lg text-lg font-semibold">
+            次の問題へ
+          </button>
+        </form>
       </div>
-    </motion.div>
+    </div>
   );
 };
 

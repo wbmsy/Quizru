4394ed7f5089abe85a089991f446bb5121b54bb5
wbmsy <177901080+wbmsy@users.noreply.github.com>
2025-02-25 21:55:17 +0900

    change text

--- src/features/quiz/components/DatabaseList.tsx
+++ src/features/quiz/components/DatabaseList.tsx
@@ -9,7 +9,7 @@ const DatabaseList: React.FC<{ QuizDatabase: QuizDatabase[] }> = ({
     <>
       <div className="bg-white rounded-lg shadow-xl p-8 w-[90%] max-w-[1000px] max-h-[80%] mx-auto border border-gray-100">
         <h2 className="text-4xl font-bold text-gray-800 mb-6 text-center">
-          データベース一覧
+          クイズ一覧
         </h2>
         <div className="max-h-[90%] grid grid-flow-row grid-cols-1 md:grid-cols-2 place-items-center overflow-auto">
           {QuizDatabase.map((item, index) => (
